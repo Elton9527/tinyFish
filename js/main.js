c04786379b0ce1780d7a13233c6c0ae091faaa8f
Elton <wgaobo@sina.cn>
2018-05-28 11:27:26 +0800

    绘制背景

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var can1;
+var can2;
+
+var ctx1;
+var ctx2;
+
+var lastTime;  // 上一帧执行的时间
+var deltaTime; // 两帧间隔的时间差
+
+var bgPic = new Image();
+
+var canWidth;  // 画布宽度
+var canHeight; // 画布高度
+
+// 页面加载完后，执行game
+document.body.onload = game;
+function game(){
+	init();
+	lastTime = Date.now();
+	deltaTime = 0;
+	gameloop();
+}
+
+function init(){
+	// 获得canvas context
+	can1 = document.getElementById("canvas1");
+	ctx1 = can1.getContext("2d");
+	can2 = document.getElementById("canvas2");
+	ctx2 = can2.getContext("2d");
+
+	bgPic.src = "./src/background.jpg";
+	canWidth = can1.width;
+    canHeight = can1.height;
+}
+
+function gameloop(){
+	requestAnimFrame(gameloop);
+	var now = Date.now();
+	deltaTime = now - lastTime;
+	lastTime = now;
+	drawBackground();
+	//console.log(deltaTime);
+}
